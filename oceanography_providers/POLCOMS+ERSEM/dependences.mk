d741687a5965ca791939ff2da71fc94cc4a694ea
Mark R Payne <markpayneatwork@users.noreply.github.com>
2010-11-19 00:59:31 +0000

    Removed build files from the POLCOMS+ERSEM and zooplankton_test pbis

--- oceanography_providers/POLCOMS+ERSEM/dependences.mk
+++
@@ -1,8 +0,0 @@
-monthly_mean_test.mod : time_tools.mod constants.mod run_context.mod input_parser.mod netcdf.mod 
-monthly_mean_test.o : time_tools.mod constants.mod run_context.mod input_parser.mod netcdf.mod 
-monthly_mean_test_v0.mod : time_tools.mod constants.mod run_context.mod input_parser.mod netcdf.mod 
-monthly_mean_test_v0.o : time_tools.mod constants.mod run_context.mod input_parser.mod netcdf.mod 
-test_local2.mod : netcdf.mod netcdf.mod 
-test_local2.o : netcdf.mod netcdf.mod 
-test_local_netcdf4.mod : netcdf.mod netcdf.mod 
-test_local_netcdf4.o : netcdf.mod netcdf.mod 

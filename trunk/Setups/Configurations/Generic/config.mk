132325b49d17659120d3648c30dcf15332273f61
asch <asch@87730626-a2f3-4109-9f2c-9704a434d394>
2011-01-06 16:30:36 +0000

    Added this directory to contain data/parameters/scripts for configuring IBMlib. In the future, BuildTools is reserved for "tools", e.g. code preprocessors

---
+++ trunk/Setups/Configurations/Generic/config.mk
@@ -0,0 +1,22 @@
+# -----------------------------------------------------------------
+# config.mk should define these 4 targets:
+#
+#    EXECUTABLE          : name of the final executable
+#    PHYSICAL_FIELDS_DIR : directory of the PHYSICAL_FIELDS interface
+#    PARTICLE_STATE_DIR  : directory of the PARTICLE_STAT interface
+#    TASK_DIR            : directory of the TASK interface
+#
+#    (each directory above referenced to the base library)
+# -----------------------------------------------------------------
+EXECUTABLE          = ibmrun
+#
+PHYSICAL_FIELDS_DIR = oceanography_providers/linear_field
+#PHYSICAL_FIELDS_DIR = oceanography_providers/POLCOMS+ERSEM
+#PHYSICAL_FIELDS_DIR = oceanography_providers/SUNFISH
+#
+PARTICLE_STATE_DIR  = biology_providers/passive
+#PARTICLE_STATE_DIR  = biology_providers/sandeel_simple
+# 
+#TASK_DIR            = task_providers/test
+TASK_DIR            = task_providers/basic_simulation
+#

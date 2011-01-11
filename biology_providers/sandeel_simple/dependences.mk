01e10d69c9904c1dfbdbc893deca5989622e19cb
Asbjorn Christensen <asbjorn-christensen@users.noreply.github.com>
2011-01-11 23:16:47 +0000

    added get_property interface to remaining biology providers
          interface get_property
            module procedure get_prop_state
          end interface
          public :: get_property
          public :: get_metadata_state
          public :: get_particle_version

--- biology_providers/sandeel_simple/dependences.mk
+++ biology_providers/sandeel_simple/dependences.mk
@@ -1,4 +1,4 @@
-sandeel.mod : time_tools.mod particle_tracking.mod physical_fields.mod run_context.mod input_parser.mod 
-sandeel.o : time_tools.mod particle_tracking.mod physical_fields.mod run_context.mod input_parser.mod 
+sandeel.mod : time_tools.mod particle_tracking.mod physical_fields.mod run_context.mod input_parser.mod output.mod 
+sandeel.o : time_tools.mod particle_tracking.mod physical_fields.mod run_context.mod input_parser.mod output.mod 
 sandeel_orig_version.mod : physical_fields.mod particle_tracking.mod run_context.mod input_parser.mod time_tools.mod 
 sandeel_orig_version.o : physical_fields.mod particle_tracking.mod run_context.mod input_parser.mod time_tools.mod 

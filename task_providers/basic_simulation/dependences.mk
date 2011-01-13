6e355f2cc4a7e998df83a6bac9cea2942fa741d3
Mark R Payne <markpayneatwork@users.noreply.github.com>
2011-01-13 15:23:07 +0000

    Basic_simulation task provider: Modified to work with output version. There was an error with the choice of the time variable, where it was being specified as sourceBox rather than POSIX. Modifications to build system so that output builds. Removed files that shouldn't be under version control

--- task_providers/basic_simulation/dependences.mk
+++
@@ -1,2 +0,0 @@
-basic_simulation.mod : input_parser.mod time_tools.mod run_context.mod physical_fields.mod particles.mod 
-basic_simulation.o : input_parser.mod time_tools.mod run_context.mod physical_fields.mod particles.mod 

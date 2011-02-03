c4d1e377e71c7bf00f7baa9fea53d7d6578548e0
Asbjorn Christensen <asbjorn-christensen@users.noreply.github.com>
2011-02-03 15:05:15 +0000

    moved search_sorted_list, which is being duplicated many times, to a new module array_tools.f

---
+++ array_tools.f
@@ -0,0 +1,54 @@
+ccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+c     ---------------------------------------------------
+c     Misc generic array tools
+c     ---------------------------------------------------
+c     $Rev: $
+c     $LastChangedDate:  $
+c     $LastChangedBy: $  
+c   
+ccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+      module array_tools
+      implicit none
+      private
+      public ::  search_sorted_list
+
+c     ---------
+      contains
+c     ---------      
+
+      subroutine search_sorted_list(v,vlist,i)
+c     ------------------------------------------------------
+c     Locate the position i in sorted list vlist where v should
+c     appear so that  vlist(i) < v < vlist(i+1)
+c     Especially return i=0, if  v < vlist(1) and
+c     return i=n, if  v > vlist(n)
+c     Search by bisection
+c     ------------------------------------------------------
+      real, intent(in)     :: v, vlist(:)
+      integer, intent(out) :: i
+      integer              :: ip,itest,n
+c     ------------------------------------------------------
+      n = size(vlist)
+      if (v < vlist(1)) then
+         i=0
+         return
+      endif
+      if (v > vlist(n)) then
+         i=n
+         return
+      endif
+c     --- do loop: keep vlist(i) < v < vlist(ip) and i<ip
+      i  = 1  ! we know v > vlist(1)
+      ip = n  ! we know v < vlist(1)
+      do while ((ip-i)>1)
+         itest = (i+ip)/2 ! integer arithmetics: i<itest<ip
+         if (vlist(itest)>v) then
+            ip = itest
+         else
+            i  = itest
+         endif
+      enddo
+c     ------------------------------------------------------
+      end subroutine search_sorted_list
+
+      end module 
--- oceanography_providers/generic_elements/mesh_grid.f
+++ oceanography_providers/generic_elements/mesh_grid.f
@@ -39,7 +39,7 @@ cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
       use time_tools           ! import clock type
       use constants
       use geometry
-      
+      use array_tools
 
       implicit none
       private           ! default scope
@@ -68,7 +68,6 @@ c      public :: interpolate_wind    ! currently unused
       
 c     ---------- other exports ----------
 
-      public :: search_sorted_list
       public :: get_ncc_coordinates
 
 c     -------------------- module data --------------------  
@@ -408,42 +407,6 @@ c     ------------------------------------------
 c     ------------------------------------------ 
       end subroutine 
 
-
-
-      subroutine search_sorted_list(v,vlist,i)
-c     ------------------------------------------------------
-c     Locate the position i in sorted list vlist where v should
-c     appear so that  vlist(i) < v < vlist(i+1)
-c     Especially return i=0, if  v < vlist(1) and
-c     return i=n, if  v > vlist(n)
-c     Search by bisection
-c     ------------------------------------------------------
-      real, intent(in)     :: v, vlist(:)
-      integer, intent(out) :: i
-      integer              :: ip,itest,n
-c     ------------------------------------------------------
-      n = size(vlist)
-      if (v < vlist(1)) then
-         i=0
-         return
-      endif
-      if (v > vlist(n)) then
-         i=n
-         return
-      endif
-c     --- do loop: keep vlist(i) < v < vlist(ip) and i<ip
-      i  = 1  ! we know v > vlist(1)
-      ip = n  ! we know v < vlist(1)
-      do while ((ip-i)>1)
-         itest = (i+ip)/2 ! integer arithmetics: i<itest<ip
-         if (vlist(itest)>v) then
-            ip = itest
-         else
-            i  = itest
-         endif
-      enddo
-c     ------------------------------------------------------
-      end subroutine search_sorted_list
 
 
 

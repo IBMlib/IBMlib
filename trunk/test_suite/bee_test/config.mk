6adbca29b61589ca6e69a53dd3bf0c5f96a0a150
mpay <mpay@87730626-a2f3-4109-9f2c-9704a434d394>
2011-02-18 08:17:08 +0000

    Bee_test: Tidied up bee_test, added README, added keywords to all

--- trunk/test_suite/bee_test/config.mk
+++ trunk/test_suite/bee_test/config.mk
@@ -1,7 +1,7 @@
 ##################################################################
 #              M a i n   c o n f i g u r a t i o n               #
 ##################################################################
-EXECUTABLE          = ibmrun_test   # name of the executable to build
+EXECUTABLE          = bee_test   # name of the executable to build
 PHYSICAL_FIELDS_DIR = $(IBMLIB_DIR)/oceanography_providers/linear_field
 PARTICLE_STATE_DIR  = $(IBMLIB_DIR)/biology_providers/passive
 TASK_DIR            = $(IBMLIB_DIR)/test_suite/bee_test

a75729c12479f7afbae039b851fb949b37ea9175
asch <asch@87730626-a2f3-4109-9f2c-9704a434d394>
2011-04-04 10:52:59 +0000

    This self test tests for:   1) trajectory integration accuracy   2) vertical well-mixed condition for depth << sqrt(2*vdiff*tsim)

---
+++ trunk/test_suite/vertical_homogenity/config.mk
@@ -0,0 +1,9 @@
+##################################################################
+#              M a i n   c o n f i g u r a t i o n               #
+##################################################################
+EXECUTABLE          = ibmrun_test   # name of the executable to build
+PHYSICAL_FIELDS_DIR = $(IBMLIB_DIR)/oceanography_providers/vortex1
+PARTICLE_STATE_DIR  = $(IBMLIB_DIR)/biology_providers/passive
+TASK_DIR            = $(IBMLIB_DIR)/test_suite/vertical_homogenity
+##################################################################
+

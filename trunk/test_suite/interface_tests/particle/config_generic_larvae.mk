6dc4fe79ff3fb59d122cb8222d3202c09d3085cb
asch <asch@87730626-a2f3-4109-9f2c-9704a434d394>
2011-04-06 13:07:18 +0000

    connected this sub test to the full test suite

---
+++ trunk/test_suite/interface_tests/particle/config_generic_larvae.mk
@@ -0,0 +1,9 @@
+##################################################################
+#              M a i n   c o n f i g u r a t i o n               #
+##################################################################
+EXECUTABLE          = ibmrun_test   # name of the executable to build
+PHYSICAL_FIELDS_DIR = $(IBMLIB_DIR)/oceanography_providers/linear_field
+PARTICLE_STATE_DIR  = $(IBMLIB_DIR)/biology_providers/generic_larvae
+TASK_DIR            = $(IBMLIB_DIR)/test_suite/interface_tests/particle
+##################################################################
+

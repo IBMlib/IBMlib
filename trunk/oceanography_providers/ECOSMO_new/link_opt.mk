c8b4fe36d9533845d9b962213f0e562bcb20acdb
asch <asch@87730626-a2f3-4109-9f2c-9704a434d394>
2011-06-23 14:30:55 +0000

    Initial commit of new ECOSMO PBI

---
+++ trunk/oceanography_providers/ECOSMO_new/link_opt.mk
@@ -0,0 +1,6 @@
+#
+#  --- exported link options to final build ---
+#
+LINKFLAGS_PHYSICAL = -I/usr/local/include
+LINKLIBS_PHYSICAL  = 
+#LINKLIBS_PHYSICAL  = -L/usr/lib  -L/usr/local/lib -lnetcdff -lnetcdf  -lhdf5_hl -lhdf5 -lz

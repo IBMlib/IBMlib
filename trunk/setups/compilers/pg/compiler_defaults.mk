66465adefdfbb3cf9ea86e20e4c159aacddda923
mpay <mpay@87730626-a2f3-4109-9f2c-9704a434d394>
2013-08-21 12:07:40 +0000

    Added support for portland group compilers

---
+++ trunk/setups/compilers/pg/compiler_defaults.mk
@@ -0,0 +1,27 @@
+#ccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+# ---------------------------------------------------
+# Portland Group Fortran (pgf90) Compiler default flags 
+# ---------------------------------------------------
+# $Rev: 212 $
+# $LastChangedDate: 2011-01-24 12:08:32 +0100 (Mon, 24 Jan 2011) $
+# $LastChangedBy: mpay $ 
+#
+# compiler settings   
+# export directive faciliates that variables are passed to sub-make
+# (unless locally overwritten)
+#ccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+# compiler settings   
+export FC        = pgf90
+export FCFLAGS   =  -e90 -i4 -error_limit 3 -I$(IBMLIB_DIR)
+export FPPFLAGS  = -fpp 
+
+MODDIRS = $(PHYSICAL_FIELDS_DIR) $(PARTICLE_STATE_DIR) $(TASK_DIR) $(OUTPUT_WRITER_DIRS)
+FCFLAGS += $(addprefix -I,$(MODDIRS))  
+
+# linker settings
+LINKFLAGS = -i4
+LINKLIBS  += -L/usr/local/include 
+
+export LITTLE_ENDIAN = -convert little_endian
+export BIG_ENDIAN    = -convert big_endian

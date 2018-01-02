9ab4d8482dbccb85cf6001b712d39fa8aae6c913
Asbjorn Christensen <asc@aqua.dtu.dk>
2018-01-02 12:00:07 +0100

    point to SRAAM setup

--- config.mk
+++ config.mk
@@ -1,4 +1,8 @@
+#EXECUTABLE          = ibmrun.exe
 EXECUTABLE          = ibmrun
-PHYSICAL_FIELDS_DIR = $(IBMLIB_DIR)/oceanography_providers/linear_field
-PARTICLE_STATE_DIR  = $(IBMLIB_DIR)/biology_providers/passive
-TASK_DIR            = $(IBMLIB_DIR)/task_providers/basic_simulation
+#PHYSICAL_FIELDS_DIR = $(IBMLIB_DIR)/oceanography_providers/OPEC
+PHYSICAL_FIELDS_DIR = $(IBMLIB_DIR)/oceanography_providers/ECOSMO_new
+#PHYSICAL_FIELDS_DIR = $(IBMLIB_DIR)/oceanography_providers/BIMS-ECO
+PARTICLE_STATE_DIR  = $(IBMLIB_DIR)/biology_providers/physical_traits_simple
+TASK_DIR            = $(IBMLIB_DIR)/task_providers/connectivity
+

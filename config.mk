473e29405a310c304d23b1f8a7370e28619f1940
Asbjorn Christensen <asc@aqua.dtu.dk>
2018-01-02 11:44:56 +0100

    point to SRAAM setup

---
+++ config.mk
@@ -0,0 +1,4 @@
+EXECUTABLE          = ibmrun
+PHYSICAL_FIELDS_DIR = $(IBMLIB_DIR)/oceanography_providers/linear_field
+PARTICLE_STATE_DIR  = $(IBMLIB_DIR)/biology_providers/passive
+TASK_DIR            = $(IBMLIB_DIR)/task_providers/basic_simulation
